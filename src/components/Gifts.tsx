a1dcb21a08256f84937b4097fe46cdeab817bc28
Nelson Fleig <nfleig@hotmail.com>
2026-03-16 00:18:59 +0100

    fix gift section spacing

--- src/components/Gifts.tsx
+++ src/components/Gifts.tsx
@@ -5,7 +5,7 @@ import { illustrations } from "../constants";
 const Gifts = () => {
   return (
     <motion.section
-      className="relative max-w-3xl mx-auto px-6 py-20 pb-30 text-center overflow-hidden"
+      className="relative  overflow-hidden"
       variants={stagger}
       initial="hidden"
       whileInView="show"
@@ -17,34 +17,36 @@ const Gifts = () => {
         aria-hidden="true"
         className="absolute bottom-0 right-0 w-40 lg:w-52 pointer-events-none select-none rounded-none"
       />
-      <div className="space-y-12">
-        <motion.h2 variants={fadeUp} className="mb-8">
-          Sugerencia de regalo
-        </motion.h2>
+      <div className="max-w-3xl mx-auto px-6 py-20 pb-30 text-center">
+        <div className="space-y-12">
+          <motion.h2 variants={fadeUp} className="mb-8">
+            Sugerencia de regalo
+          </motion.h2>
 
-        <motion.p variants={fadeUp} className="mb-12">
-          Su presencia es el mejor regalo que podemos recibir. Pero si desean
-          hacernos un obsequio, pueden contribuir a nuestro nuevo hogar a través
-          de los siguientes medios.
-        </motion.p>
+          <motion.p variants={fadeUp} className="mb-12">
+            Su presencia es el mejor regalo que podemos recibir. Pero si desean
+            hacernos un obsequio, pueden contribuir a nuestro nuevo hogar a
+            través de los siguientes medios.
+          </motion.p>
 
-        <motion.div variants={fadeUp}>
-          <h3 className="mb-4">Bizum</h3>
-          <p>+34 625 99 55 39</p>
-        </motion.div>
+          <motion.div variants={fadeUp}>
+            <h3 className="mb-4">Bizum</h3>
+            <p>+34 625 99 55 39</p>
+          </motion.div>
 
-        <motion.div variants={fadeUp}>
-          <h3 className="mb-4">PayPal</h3>
-          <p>nelson.fleig@gmail.com</p>
-        </motion.div>
+          <motion.div variants={fadeUp}>
+            <h3 className="mb-4">PayPal</h3>
+            <p>nelson.fleig@gmail.com</p>
+          </motion.div>
 
-        <motion.div variants={fadeUp}>
-          <h3 className="mb-4">Transferencia bancaria</h3>
-          <p>Banco: Mercantil Santa Cruz</p>
-          <p>Cta Ahorros (Bolivianos) 4067788562</p>
-          <p>María Paula Baldiviezo</p>
-          <p>CI 4707116 SC</p>
-        </motion.div>
+          <motion.div variants={fadeUp}>
+            <h3 className="mb-4">Transferencia bancaria</h3>
+            <p>Banco: Mercantil Santa Cruz</p>
+            <p>Cta Ahorros (Bolivianos) 4067788562</p>
+            <p>María Paula Baldiviezo</p>
+            <p>CI 4707116 SC</p>
+          </motion.div>
+        </div>
       </div>
     </motion.section>
   );
